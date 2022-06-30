e3745fae67d93d39b27008759adadb6e3669f9d9
FelisDiligens <47528453+FelisDiligens@users.noreply.github.com>
2022-06-30 11:43:55 +0200

    Added TextboxWriter

---
+++ Fo76ini/Utilities/TextboxWriter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Fo76ini.Utilities
+{
+    public class TextboxWriter : TextWriter
+    {
+        public TextBox TextBox { get; set; }
+
+        public override Encoding Encoding => Encoding.Default;
+        public override string NewLine => "\r\n";
+
+        public TextboxWriter(TextBox textBox)
+        {
+            TextBox = textBox;
+        }
+
+        // https://stackoverflow.com/a/17712686
+        public override void Write(char value)
+        {
+            TextBox.Invoke(new Action(() => {
+                TextBox.AppendText(value.ToString());
+            }));
+        }
+
+        public override void Write(string value)
+        {
+            TextBox.Invoke(new Action(() => {
+                TextBox.AppendText(value);
+            }));
+        }
+
+        public override void WriteLine(string value)
+        {
+            TextBox.Invoke(new Action(() => {
+                TextBox.AppendText(value + NewLine);
+            }));
+        }
+    }
+}
