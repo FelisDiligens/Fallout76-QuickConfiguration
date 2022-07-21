a5253aabb25f1b1821de883e58c8ac12057140bb
FelisDiligens <47528453+FelisDiligens@users.noreply.github.com>
2022-07-21 14:00:03 +0200

    Version bump v1.10.2

--- Fo76ini/Shared.cs
+++ Fo76ini/Shared.cs
@@ -10,7 +10,7 @@ namespace Fo76ini
     /// </summary>
     public class Shared
     {
-        public const string VERSION = "1.10.1";
+        public const string VERSION = "1.10.2";
         public static string LatestVersion = null;
 
         public static readonly string AppInstallationFolder = Directory.GetParent(Application.ExecutablePath).ToString();
